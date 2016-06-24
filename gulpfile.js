10654fc974824b6bec295290b94ff103b52886fa
Julien G <julien@g-u-i.net>
2016-06-24 17:53:25 +0200

    Fix issue #13

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ gulp.task('css', ['less'], function (done) {
   return gulp.src([
       'assets/css/*.css',
     ])
-    .pipe(nano({discardComments: {removeAll: true}}))
+    .pipe(nano({discardComments: {removeAll: true}, autoprefixer: false}))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('assets/production'));
 });
