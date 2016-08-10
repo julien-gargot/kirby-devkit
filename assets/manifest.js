b1e8783268b6f9029ef36fdad931626eb820dd48
Julien Gargot <julien@g-u-i.me>
2016-08-10 18:35:39 +0200

    Update manifest comments

--- assets/manifest.js
+++ assets/manifest.js
@@ -4,28 +4,32 @@
 User settings
 ---------------------------------------
 
-List all your JavaScript file in `userScripts` to define
-their order of concatenation.
-
-List all the JavaScript plugin files you are using in `pluginScripts`
-to define their loading order.
-
-Name your LESS config file to load.
-Managing more than one LESS/CSS should be from @imports in LESS.
-
-To enable automatic reloading on .js and .less files compilation,
-as well as other niceties from [browser sync](https://www.browsersync.io/)
-write your local dev url in the localDevUrl variable.
-
 */
+
+/* List all the JavaScript plugin files you are using in `pluginScripts`
+ * to define their loading order.
+ */
 module.exports.pluginScripts = [
   'bower_components/jquery/dist/jquery.js',
   'bower_components/bootstrap/dist/js/bootstrap.js'
 ];
+
+/* List all your JavaScript file in `userScripts` to define
+ * their order of concatenation.
+ */
 module.exports.userScripts = [
   'assets/js/main.js'
 ];
+
+/* Name your LESS config file to load.
+ * Managing more than one LESS/CSS should be from @imports in LESS.
+ */
 module.exports.userStyles = [
   'assets/less/main.less'
 ];
+
+/* To enable automatic reloading on .js and .less files compilation,
+ * as well as other niceties from [browser sync](https://www.browsersync.io/)
+ * write your local dev url in the localDevUrl variable.
+ */
 module.exports.localDevUrl = '';
